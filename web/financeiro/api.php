827e1ed7f3cd39ec47f6f39c1014f60ae4591583
jbsalgado <jbsalgado2@gmail.com>
2026-06-05 13:07:19 -0300

    ajustes

--- web/financeiro/api.php
+++ web/financeiro/api.php
@@ -1,13 +1,14 @@
 <?php
-// Define constants to bootstrap Yii2
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+// Define constants to bootstrap Yii2 - commented out for production compatibility
+// defined('YII_DEBUG') or define('YII_DEBUG', true);
+// defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../../config/web.php';
 new yii\web\Application($config);
+Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
 
 // Set header to JSON
 header('Content-Type: application/json; charset=utf-8');
