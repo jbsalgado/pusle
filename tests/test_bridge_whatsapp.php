2668e6f31cec2e117826162bb957d4edbc201a0c
jbsalgado <jbsalgado@users.noreply.github.com>
2026-09-05 03:11:16 -0300

    fix(test): ajustar bootstrap com Dotenv e config/web.php

--- tests/test_bridge_whatsapp.php
+++ tests/test_bridge_whatsapp.php
@@ -1,10 +1,14 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->safeLoad();
+
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-$config = require __DIR__ . '/../config/console.php';
-new yii\console\Application($config);
+$config = require __DIR__ . '/../config/web.php';
+new yii\web\Application($config);
 
 use app\models\Usuario;
 use app\modules\vendas\models\BridgeWhatsappLoja;
