ced8d735d640a89edd88df97aa9b5c478aaeab07
jbsalgado <jbsalgado@users.noreply.github.com>
2026-09-05 00:54:43 -0300

    fix(test): adicionar Dotenv em test_bridge_download.php

--- tests/test_bridge_download.php
+++ tests/test_bridge_download.php
@@ -1,6 +1,10 @@
 <?php
 
 require(__DIR__ . '/../vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->safeLoad();
+
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/web.php');
